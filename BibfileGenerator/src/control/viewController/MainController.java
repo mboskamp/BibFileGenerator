22fe77c2435407181078399dc4eb397967096b60
mboskamp <mboskamp@t-online.de>
2016-09-20 12:59:45 +0200

    Layout changes

--- BibfileGenerator/src/control/viewController/MainController.java
+++ BibfileGenerator/src/control/viewController/MainController.java
@@ -10,36 +10,6 @@ import javafx.stage.Stage;
 
 public class MainController {
 	
-	@FXML
-	private Button createButton;
-	
-	@FXML
-	private Button loadButton;
-	
-	//Test
-	@FXML
-	public void load(){
-		System.out.println("Load");
-		
-		FileChooser fileChooser = new FileChooser();
-		 fileChooser.setTitle("Open Resource File");
-		 File selectedFile = fileChooser.showOpenDialog(new Stage());
-		 fileChooser.getExtensionFilters().add(new ExtensionFilter("Bibtex Files", "*.bib"));
-		 if(selectedFile != null && selectedFile.getAbsolutePath().endsWith(".bib")){
-			 System.out.println("File selected");
-			 //TODO Read file
-		 }else{
-			 System.out.println("wrong extension");
-		 }
-	}
-	
-	@FXML
-	public void create(){
-		System.out.println("Create new");
-		
-		Stage stage = (Stage) createButton.getScene().getWindow();
-	}
-	
 	//Test
 	@FXML
     public void initialize() {
