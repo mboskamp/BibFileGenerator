4619e5beb37ef5f7034657c032a3e63c5c6b24d2
mboskamp <mboskamp@t-online.de>
2016-10-10 13:52:38 +0200

    Add new Books now supported

--- BibfileGenerator/src/control/viewController/MainWindowController.java
+++ BibfileGenerator/src/control/viewController/MainWindowController.java
@@ -2,20 +2,43 @@ package control.viewController;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import org.jbibtex.BibTeXEntry;
+import org.jbibtex.Entry;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import model.GlobalStorage;
 
-public class MainWindowController {
+public class MainWindowController extends AbstractController{
+	
+	private ArrayList<Entry> entries = new ArrayList<>();
+	
+	@FXML
+	public TableView<Entry> table;
 	
 	//Test
 	@FXML
     public void initialize() {
 		
+		for (String s : BibTeXEntry.getKeysAsString()) {
+			TableColumn<Entry, String> column = new TableColumn<>();
+			column.setText(s);
+			column.setCellValueFactory(
+					new PropertyValueFactory<Entry, String>(s));
+			
+			table.getColumns().add(column);			
+		}
     }
 	
 	public void add(){
@@ -23,6 +46,10 @@ public class MainWindowController {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/NewEntryWindow.fxml"));
 			Parent root1;
 			root1 = (Parent) fxmlLoader.load();
+			
+			AbstractController c = fxmlLoader.getController();
+			c.setFrom(this);
+			
 			Stage stage = new Stage();
 			stage.initModality(Modality.APPLICATION_MODAL);
 			stage.setResizable(false);
@@ -32,6 +59,14 @@ public class MainWindowController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void notifyAdd(String key){
+		BibTeXEntry entry = (BibTeXEntry) GlobalStorage.getInstance().get(key);
+		this.entries.add(entry.getEntry());
+		
+		ObservableList<Entry> entries =  FXCollections.observableArrayList(this.entries);
+		table.setItems(entries);
 	}
 	
 	public void open(){
